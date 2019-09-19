bdc7993db41479261bdb6590daa6ff7a259c696c
Luke <deandrevidal@aol.com>
2019-09-18 19:06:47 -0700

    is_word_guessed func converted from spaceman 1

--- spaceman.py
+++ spaceman.py
@@ -28,7 +28,12 @@ def is_word_guessed(secret_word, letters_guessed):
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
     '''
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+    for letter in secret_word:
+        if letter in letters_guessed:
+            pass
+        else:
+            return False
+    return True
 
 
 def get_guessed_word(secret_word, letters_guessed):
