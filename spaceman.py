b1198c9c9ba52162249b13d1fab0f4d85acf4304
Luke <deandrevidal@aol.com>
2019-09-18 19:08:52 -0700

    get_guessed_word func converted from spaceman 1

--- spaceman.py
+++ spaceman.py
@@ -48,7 +48,13 @@ def get_guessed_word(secret_word, letters_guessed):
 
     # TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
 
-    pass
+    result = ""
+    for letter in secret_word:
+        if letter in letters_guessed:
+            result += letter
+        else:
+            result += "_"
+    return result
 
 
 def is_guess_in_word(guess, secret_word):
