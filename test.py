66850f562e69ed03560ecec37485e00e43e30836
Luke <deandrevidal@aol.com>
2019-09-18 20:48:10 -0700

    set up unittest, tests only pass if replay option commented out, fix later

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 import unittest
-from spaceman import is_valid, is_guess_in_word, is_word_guessed
+from spaceman import is_valid, is_guess_in_word, is_word_guessed, get_guessed_word
+
+
+def test_is_guess_in_word():
+
+    assert is_guess_in_word(
+        't', 'test') == True, 'is_guess_in_word() Error: new input in letters_guessed matches secret_word'
+
+
+if '__name__' == '__main__':
+    test_is_guess_in_word()
